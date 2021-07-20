43099ee44adf64c69c0e0ecd264046dc1e2be32b
Igor Magalhães <igor.magalhaes.r@gmail.com>
2021-07-20 18:12:35 -0300

    new framelist added

---
+++ dexter/framelist.py
@@ -0,0 +1,31 @@
+"""
+FrameList
+---------
+Data in the form of a list of dataframes.
+
+"""
+
+from dexter.helper import _to_html_str_
+
+
+class FrameList(list):
+    """
+    Data in the form of a list of dataframes.
+
+    Parameters
+    ----------
+    frames: List containing pandas dataframes.
+
+    Example
+    -------
+    >>> dataframes = FrameList([df1, df2, df3])
+    >>> dataframes
+    FrameList([df1, df2, df3])
+    _______
+    """
+
+    def _repr_html_(self):
+        """
+        Return a HTML representation for a FrameMap
+        """
+        return _to_html_str_(self)
